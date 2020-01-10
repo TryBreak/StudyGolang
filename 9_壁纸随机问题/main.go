913f74140818942c008e388368344204e158c7b1
Mark <meichangliang@outlook.com>
2020-01-11 01:51:48 +0800

    add

--- 9_壁纸随机问题/main.go
+++ 9_壁纸随机问题/main.go
@@ -1,6 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 func main() {
+	rand.Seed(time.Now().UnixNano())
 
 	/*
 		一共有26张图片 , 两个一组进行展示
@@ -11,7 +18,7 @@ func main() {
 	*/
 
 	// 构建壁纸集合 0 代表Yoda
-	bzhi := []int{}
+
 	for i := 1; i <= 26; i++ {
 		if i >= 23 {
 			bzhi = append(bzhi, 0)
@@ -19,6 +26,46 @@ func main() {
 			bzhi = append(bzhi, i)
 		}
 	}
-	//进行递归循环,直到
+	/*
+		1. 生成一组随机下标
+		2. 如果下标相等则重新生成直到不等
+		3. 随即范围是 0-25
+	*/
+	/*
+		根据随机数遍历图片数组,都为0则停下 , 记录一次
+	*/
+	for index := 0; index < 100; index++ {
+		start()
+	}
+	//求平均数
+	allNum := 0
+	for index := 0; index < len(law); index++ {
+		allNum += law[index]
+	}
+	fmt.Printf("则百次匹配Yoda,平均用了%v次", allNum/len(law))
 
+}
+
+var (
+	bzhi []int
+	law  []int
+)
+
+func start() {
+	num := 0
+	for {
+		a := rand.Intn(25)
+		b := rand.Intn(25)
+		if a != b {
+			num++
+			bzhiA := bzhi[a]
+			bzhiB := bzhi[b]
+			// fmt.Printf("第一次匹配,壁: %v ; 壁纸: %v \n", bzhiA, bzhiB)
+			if bzhiA == bzhiB {
+				// fmt.Printf("成功匹配到两个Yoda , 一共用了%v次 \n", num)
+				law = append(law, num)
+				break
+			}
+		}
+	}
 }
