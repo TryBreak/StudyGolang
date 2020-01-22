8871a9320b4f60e2baa512378648b0c4939c995a
Mark <meichangliang@outlook.com>
2020-01-23 01:26:06 +0800

    add

--- 15_结构体/main.go
+++ 15_结构体/main.go
@@ -2,19 +2,31 @@ package main
 
 import "fmt"
 
-/*
-	自定义类型和类型别名:
-	基于内置的类型造一个自己的类型
-
-*/
-
-// type 后面跟的是一个类型 , 自定义类型例子:
-type myInt int
-
-//类型别名
-type yourInt = int
-
 func main() {
+	// typeDemo()
+}
+
+func typeDemo() {
+
+	/*
+		自定义类型和类型别名:
+		基于内置的类型造一个自己的类型
+
+	*/
+
+	// type 后面跟的是一个类型 , 自定义类型例子:
+
+	/*
+
+	   区别,:
+	   自定义类型是自己的类型
+	   类型别名只在编写过程中有效
+
+	*/
+	type myInt int
+
+	//类型别名
+	type yourInt = int
 	var n myInt
 	n = 100
 	fmt.Printf("%T\n", n) // main.myInt
@@ -28,11 +40,3 @@ func main() {
 	fmt.Printf("%T\n", c)
 
 }
-
-/*
-
-区别,:
-自定义类型是自己的类型
-类型别名只在编写过程中有效
-
-*/
