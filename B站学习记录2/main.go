d57c07956179e2ea4ca1c8cb8cf399687ee52510
墨七 <meichangliang@mo7.cc>
2021-11-30 15:47:16 +0800

    add

--- B站学习记录2/main.go
+++ B站学习记录2/main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Hello World1")
+	fmt.Println("Hello World2")
+	fmt.Println("Hello World3")
+	var age = 19
+	fmt.Println(age)
 }
