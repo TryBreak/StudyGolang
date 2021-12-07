29e538e6fa0020a547884d7979d4ce73387b16f7
墨七 <meichangliang@mo7.cc>
2021-12-07 13:53:31 +0800

    add

--- B站学习记录2/6：包/go.mod
+++ B站学习记录2/6：包/go.mod
@@ -1,3 +1,3 @@
-module test
+module test.com
 
 go 1.17
--- B站学习记录2/6：包/main.go
+++ B站学习记录2/6：包/main.go
@@ -1,8 +1,10 @@
-package main
+package main // package 进行包的声明，建议：包的声明这个包和所在的文件夹同名
+// 2. main 包是程序的入口包  一般 main 函数会放在这个包下面
 
 import (
 	"fmt"
-	test "test/test"
+
+	test "test.com/test"
 )
 
 func main() {
