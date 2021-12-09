3c310c7a8b749d27bc7c970b2da7870a16f7bbed
墨七 <meichangliang@mo7.cc>
2021-12-09 16:04:31 +0800

    add

--- B站学习记录2/7：标准库/日期时间函数.go
+++ B站学习记录2/7：标准库/日期时间函数.go
@@ -3,8 +3,27 @@
 */
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("----日期跟时间函数------")
+
+	var now time.Time = time.Now()
+
+	fmt.Printf("%v ~~ 对应的类型是：%T \n", now, now)
+	fmt.Printf("年：%v \n", now.Year())
+	fmt.Printf("月：%v \n", int(now.Month()))
+	fmt.Printf("日：%v \n", now.Day())
+	fmt.Printf("时：%v \n", now.Hour())
+	fmt.Printf("分：%v \n", now.Minute())
+	fmt.Printf("秒：%v \n", now.Second())
+
+	fmt.Println("----指定格式------")
+	t := now.Format("2006-01-02 15:04:05")
+
+	fmt.Printf("t 的值为：%v \n", t)
+	// 20060102150405
 }
