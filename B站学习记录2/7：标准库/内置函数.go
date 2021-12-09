af0dde027fafb659e77ecf3ff469d9e4a88d3f37
墨七 <meichangliang@mo7.cc>
2021-12-09 16:20:52 +0800

    add

---
+++ B站学习记录2/7：标准库/内置函数.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("内置函数", len("asdasd"))
+	/*
+		内置函数就是不需要导入包的函数
+	*/
+	fmt.Println("---new 分配内存-----")
+	// 返回该内存新分配的零值的指针
+
+	var n1 *int = new(int)
+	fmt.Println("n1", n1)
+
+}
