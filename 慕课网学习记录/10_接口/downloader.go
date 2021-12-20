4d5442ba329d4ac2c7b84076bfca737b638dc031
墨七 <meichangliang@mo7.cc>
2021-12-20 18:03:41 +0800

    add

--- 慕课网学习记录/10_接口/downloader.go
+++ 慕课网学习记录/10_接口/downloader.go
@@ -21,6 +21,20 @@ type retriever interface {
 	Get(string) string
 }
 
+/*
+
+type -- 定义自定义类型
+
+retriever -- 自定义类型的名字
+
+interface -- 接口
+		方法的集合
+
+struct -- 结构体
+		字段的集合
+
+*/
+
 func main() {
 	var r retriever = getRetriever()
 	// retriever := getRetriever()
