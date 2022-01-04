7767b442df06c0750521ee8c1fa5a05c65bf9dad
墨七 <meichangliang@mo7.cc>
2022-01-04 22:53:37 +0800

    '调试'

--- 加密/SHA256.go
+++ 加密/SHA256.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+)
+
 func main() {
+	timestamp := "1641305051"
+	secret := "6E18A34A2866CE04651D75708B33727F"
+
+	message := timestamp + "GET" + "/users/self/verify"
+
+	sign := HmacSha256(message, secret)
+	fmt.Println(sign)
+}
+
+func HmacSha256(message, secret string) string {
+	h := hmac.New(sha256.New, []byte(secret))
+	h.Write([]byte(message))
+	return base64.URLEncoding.EncodeToString(h.Sum(nil))
 }
