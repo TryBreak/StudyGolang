96f70041f58d4640efabab7ed980f2d4c62f5af7
墨七 <meichangliang@mo7.cc>
2022-01-06 06:34:35 +0800

    '调试'

---
+++ channel/channel.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	chnaO := make(chan int)
+
+	go func() {
+		n := 0
+		for {
+			n += 1
+			time.Sleep(time.Second * 3)
+			if n > 3 {
+				close(chnaO)
+			} else {
+				chnaO <- n
+			}
+		}
+	}()
+
+	go func() {
+		n := 0
+		for {
+			n += 1
+			time.Sleep(time.Second * 4)
+			if n > 3 {
+				close(chnaO)
+			} else {
+				chnaO <- n
+			}
+		}
+	}()
+
+	go func() {
+		for k := range chnaO {
+			fmt.Println("1接收到了K", k)
+		}
+		fmt.Println("1进程结束")
+	}()
+
+	go func() {
+		for k := range chnaO {
+			fmt.Println("2接收到了K", k)
+		}
+		fmt.Println("2进程结束")
+	}()
+
+	time.Sleep(time.Second * 5000)
+}
