201baae731b9587d47f8f364822f97af03feee4a
墨七 <meichangliang@mo7.cc>
2022-02-07 16:18:56 +0800

    调试

--- wss练习/clean.go
+++ wss练习/clean.go
@@ -10,7 +10,8 @@ import (
 // https://blog.csdn.net/ALakers/article/details/111713405
 
 func main() {
-	url := "ws://hunt.mo7.cc/api/wss" // 服务器地址
+	// url := "ws://hunt.mo7.cc/api/wss" // 服务器地址
+	url := "ws://localhost:9000/api/wss" // 服务器地址
 	ws, _, err := websocket.DefaultDialer.Dial(url, nil)
 	if err != nil {
 		log.Fatal(err)
