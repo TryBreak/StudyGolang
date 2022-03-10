bc20038e5eb2ad344e169b3b91bf8ea3028c9195
墨七 <meichangliang@mo7.cc>
2022-03-10 12:43:41 +0800

    调试

--- 数组的特性研究/固定的数组长度.go
+++ 数组的特性研究/固定的数组长度.go
@@ -3,11 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	arr := make([]int, 5, 10)
+	// arr := make([]int, 5, 10)
 
-	for i := 0; i < 400; i++ {
-		arr = append(arr, i)
-		fmt.Println("-------")
-		fmt.Println(arr)
-	}
+	// for i := 0; i < 400; i++ {
+	// 	arr = append(arr, i)
+	// 	fmt.Println("-------")
+	// 	fmt.Println(arr)
+	// }
+
+	arr := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}
+
+	n := 6
+
+	fmt.Println(arr[0:n])
+	fmt.Println(arr[n:])
+
 }
