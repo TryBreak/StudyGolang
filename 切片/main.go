8a8642b295ee829fe5add6c8f8ed3993e5c8bdbe
墨七 <meichangliang@mo7.cc>
2022-03-22 22:30:57 +0800

    调试

--- 切片/main.go
+++ 切片/main.go
@@ -6,13 +6,6 @@ var L = []int{1, 2, 3, 4, 5, 6, 7, 8}
 
 func main() {
 
-	period := 3
-
-	period_list := make([]int, period)
-
-	copy(period_list, L)
-
-	fmt.Println(period_list)
-	fmt.Println(L)
+	fmt.Println(L[:len(L)-1])
 
 }
